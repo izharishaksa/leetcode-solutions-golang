7901310a0ba0d4ae2dbd1295c17b2a47eb358e0e
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-07-22 14:04:12 +0700

    Create maximum-subarray.go

---
+++ maximum-subarray.go
@@ -0,0 +1,15 @@
+package leetcode_solutions_golang
+
+//https://leetcode.com/problems/maximum-subarray/
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
