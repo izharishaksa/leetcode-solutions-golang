d10750fff78bab02c3e4b44b3b5187afbbfb83e5
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-07-22 18:52:59 +0700

    Create a-number-after-a-double-reversal.go

---
+++ a-number-after-a-double-reversal.go
@@ -0,0 +1,22 @@
+package leetcode_solutions_golang
+
+//https://leetcode.com/problems/a-number-after-a-double-reversal/
+func isSameAfterReversals(num int) bool {
+	origin := num
+	result := 0
+	for num > 0 {
+		result *= 10
+		result += num % 10
+		num /= 10
+	}
+	result2 := 0
+	for result > 0 {
+		result2 *= 10
+		result2 += result % 10
+		result /= 10
+	}
+	if result2 == origin {
+		return true
+	}
+	return false
+}
