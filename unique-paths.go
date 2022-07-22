acf4ac0ac0f50f1e5ac3d3a790d96af5da4b23c0
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-07-22 10:10:23 +0700

    Create unique-paths.go

---
+++ unique-paths.go
@@ -0,0 +1,19 @@
+package leetcode_solutions_golang
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			dp[i][j] = 1
+		}
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+
+	return dp[m-1][n-1]
+}
