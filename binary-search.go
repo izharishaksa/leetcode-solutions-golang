b5bebeb2483d2b08eabef440d65a858c6b150fbd
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-07-22 19:27:24 +0700

    Create binary-search.go

---
+++ binary-search.go
@@ -0,0 +1,20 @@
+package leetcode_solutions_golang
+
+//https://leetcode.com/problems/binary-search/
+func search2(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	pivot := (left + right) / 2
+	for right >= left {
+		if nums[pivot] == target {
+			return pivot
+		}
+		if nums[pivot] > target {
+			right = pivot - 1
+		} else {
+			left = pivot + 1
+		}
+		pivot = (left + right) / 2
+	}
+	return -1
+}
