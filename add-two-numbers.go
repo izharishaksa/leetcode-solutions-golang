83e0155fd73d833b3deda50fefc631813eec5e4d
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-07-22 19:27:21 +0700

    Create add-two-numbers.go

---
+++ add-two-numbers.go
@@ -0,0 +1,39 @@
+package leetcode_solutions_golang
+
+//https://leetcode.com/problems/add-two-numbers/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var nodes []ListNode
+	res := 0
+	for l1 != nil || l2 != nil {
+		nodes = append(nodes, ListNode{Val: res})
+		res = 0
+		index := len(nodes) - 1
+		if l1 != nil {
+			nodes[index].Val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			nodes[index].Val += l2.Val
+			l2 = l2.Next
+		}
+		if nodes[index].Val > 9 {
+			val := nodes[index].Val
+			nodes[index].Val = val % 10
+			res = val / 10
+		}
+	}
+	if res > 0 {
+		nodes = append(nodes, ListNode{Val: res})
+	}
+	for i := 1; i < len(nodes); i++ {
+		nodes[i-1].Next = &nodes[i]
+	}
+	return &nodes[0]
+}
