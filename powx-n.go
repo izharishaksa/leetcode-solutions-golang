37b6c36ffe8d4ce9c1d1388d242b2ef20edd29e3
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-08-04 01:33:33 +0700

    Create powx-n.go

---
+++ powx-n.go
@@ -0,0 +1,18 @@
+package leetcode_solutions_golang
+
+//https://leetcode.com/problems/powx-n/
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n < 0 {
+		return 1 / myPow(x, -n)
+	}
+	if n%2 == 0 {
+		return myPow(x*x, n/2)
+	}
+	return x * myPow(x*x, n/2)
+}
+
+//2^7
+//2*(2*2*2)*(2*2*2)=2*(2*2)^3
