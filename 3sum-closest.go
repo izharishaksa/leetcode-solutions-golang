3aefbcc65437246ca931c98000c0988f119f1271
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-08-04 01:44:54 +0700

    Create 3sum-closest.go

---
+++ 3sum-closest.go
@@ -0,0 +1,30 @@
+package leetcode_solutions_golang
+
+import (
+	"math"
+	"sort"
+)
+
+//https://leetcode.com/problems/3sum-closest/
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	min := nums[0] + nums[1] + nums[2]
+	for i := 0; i < len(nums)-2; i++ {
+		j, k := i+1, len(nums)-1
+		for j < k {
+			sum := nums[i] + nums[j] + nums[k]
+			if sum == target {
+				return sum
+			}
+			if sum < target {
+				j++
+			} else {
+				k--
+			}
+			if math.Abs(float64(sum)-float64(target)) < math.Abs(float64(min)-float64(target)) {
+				min = sum
+			}
+		}
+	}
+	return min
+}
