a9efb2076b4a053ba5ea2b8ebf26f333d4065292
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-01 14:24:34 +0700

    Create pacific-atlantic-water-flow_test.go

---
+++ pacific-atlantic-water-flow_test.go
@@ -0,0 +1,59 @@
+package leetcode_solutions_golang
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_pacificAtlantic(t *testing.T) {
+	type args struct {
+		heights [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test1",
+			args: args{
+				heights: [][]int{
+					{1, 2, 2, 3, 5},
+					{3, 2, 3, 4, 4},
+					{2, 4, 5, 3, 1},
+					{6, 7, 1, 4, 5},
+					{5, 1, 1, 2, 4},
+				},
+			},
+			want: [][]int{
+				{0, 4},
+				{1, 3},
+				{1, 4},
+				{2, 2},
+				{3, 0},
+				{3, 1},
+				{4, 0},
+			},
+		},
+		{
+			name: "test2",
+			args: args{
+				heights: [][]int{
+					{10, 10, 10},
+					{10, 1, 10},
+					{10, 10, 10},
+				},
+			},
+			want: [][]int{
+				{0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 2}, {2, 0}, {2, 1}, {2, 2},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pacificAtlantic(tt.args.heights); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pacificAtlantic() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
