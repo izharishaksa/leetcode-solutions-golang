f6046ade8fc95eb607a33384f38ce1e0300c9345
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-03 07:57:11 +0700

    Create n-queens-ii_test.go

---
+++ n-queens-ii/n-queens-ii_test.go
@@ -0,0 +1,29 @@
+package n_queens_ii
+
+import "testing"
+
+func Test_totalNQueens(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				n: 4,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := totalNQueens(tt.args.n); got != tt.want {
+				t.Errorf("totalNQueens() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
