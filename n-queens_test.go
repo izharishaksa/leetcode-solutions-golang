32aba52d67c107e11f2bc0b2b4cf08fbaf12c011
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-07 12:35:11 +0700

    Create n-queens_test.go

---
+++ n-queens_test.go
@@ -0,0 +1,45 @@
+package leetcode_solutions_golang
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_solveNQueens(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				n: 4,
+			},
+			want: [][]string{
+				{
+					".Q..",
+					"...Q",
+					"Q...",
+					"..Q.",
+				},
+				{
+					"..Q.",
+					"Q...",
+					"...Q",
+					".Q..",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solveNQueens(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("solveNQueens() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
