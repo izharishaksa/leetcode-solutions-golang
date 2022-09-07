297f454408a86d34ae576ecc887ebbb0c557e99a
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-07 11:38:10 +0700

    Create combination-sum-ii_test.go

---
+++ combination-sum-ii/combination-sum-ii_test.go
@@ -0,0 +1,39 @@
+package combination_sum_ii
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_combinationSum2(t *testing.T) {
+	type args struct {
+		candidates []int
+		target     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				candidates: []int{10, 1, 2, 7, 6, 1, 5},
+				target:     8,
+			},
+			want: [][]int{
+				{1, 1, 6},
+				{1, 2, 5},
+				{1, 7},
+				{2, 6},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combinationSum2(tt.args.candidates, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combinationSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
