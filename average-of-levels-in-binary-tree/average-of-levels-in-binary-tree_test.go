b016405a8481b5af844f2cae13e1fd17339f28cc
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-07 11:38:03 +0700

    Create average-of-levels-in-binary-tree_test.go

---
+++ average-of-levels-in-binary-tree/average-of-levels-in-binary-tree_test.go
@@ -0,0 +1,40 @@
+package average_of_levels_in_binary_tree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_averageOfLevels(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []float64
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				root: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 9,
+					},
+					Right: &TreeNode{
+						Val: 20,
+					},
+				},
+			},
+			want: []float64{3, 14.5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := averageOfLevels(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("averageOfLevels() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
