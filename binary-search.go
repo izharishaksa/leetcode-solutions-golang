717449351326246296cee7fc2af3e3f65b11739d
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-07 13:37:09 +0700

    Update binary-search.go

--- binary-search.go
+++ binary-search.go
@@ -1,6 +1,7 @@
+//https://leetcode.com/problems/binary-search/
+
 package leetcode_solutions_golang
 
-//https://leetcode.com/problems/binary-search/
 func search2(nums []int, target int) int {
 	left := 0
 	right := len(nums) - 1
