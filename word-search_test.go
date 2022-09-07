cc086a13e9df1b7a0173c153ced2a2e9ee9952b3
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-07 12:08:19 +0700

    Create word-search_test.go

---
+++ word-search_test.go
@@ -0,0 +1,35 @@
+package leetcode_solutions_golang
+
+import "testing"
+
+func Test_exist(t *testing.T) {
+	type args struct {
+		board [][]byte
+		word  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				board: [][]byte{
+					{'A', 'B', 'C', 'E'},
+					{'S', 'F', 'C', 'S'},
+					{'A', 'D', 'E', 'E'},
+				},
+				word: "ABCCED",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := exist(tt.args.board, tt.args.word); got != tt.want {
+				t.Errorf("exist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
