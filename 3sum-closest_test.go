97de787f4fea6a3f09db6343216cd30983483da7
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-07 11:20:07 +0700

    Create 3sum-closest_test.go

---
+++ 3sum-closest_test.go
@@ -0,0 +1,31 @@
+package leetcode_solutions_golang
+
+import "testing"
+
+func Test_threeSumClosest(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				nums:   []int{-1, 2, 1, -4},
+				target: 1,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSumClosest(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("threeSumClosest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
