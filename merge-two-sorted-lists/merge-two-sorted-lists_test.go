de4b93cd4deb7e373a09603de7555a85f62f087c
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-07 11:50:31 +0700

    Create merge-two-sorted-lists_test.go

---
+++ merge-two-sorted-lists/merge-two-sorted-lists_test.go
@@ -0,0 +1,67 @@
+package merge_two_sorted_lists
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mergeTwoLists(t *testing.T) {
+	type args struct {
+		list1 *ListNode
+		list2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				list1: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 4,
+						},
+					},
+				},
+				list2: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val: 4,
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val: 4,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeTwoLists(tt.args.list1, tt.args.list2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeTwoLists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
