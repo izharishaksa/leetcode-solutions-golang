caef57c856e3414716838ef84c27eafc945ebf8a
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-10 23:14:33 +0700

    Create coin-change_test.go

---
+++ coin-change_test.go
@@ -0,0 +1,47 @@
+package leetcode_solutions_golang
+
+import "testing"
+
+func Test_coinChange(t *testing.T) {
+	type args struct {
+		coins  []int
+		amount int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				coins:  []int{1, 2, 5},
+				amount: 11,
+			},
+			want: 3,
+		},
+		{
+			name: "test case 2",
+			args: args{
+				coins:  []int{2},
+				amount: 3,
+			},
+			want: -1,
+		},
+		{
+			name: "test case 3",
+			args: args{
+				coins:  []int{1},
+				amount: 0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := coinChange(tt.args.coins, tt.args.amount); got != tt.want {
+				t.Errorf("coinChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
