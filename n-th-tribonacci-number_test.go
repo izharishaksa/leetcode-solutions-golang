b07b9a840adde22249511cc37506762b27357880
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-11 00:43:44 +0700

    Create n-th-tribonacci-number_test.go

---
+++ n-th-tribonacci-number_test.go
@@ -0,0 +1,29 @@
+package leetcode_solutions_golang
+
+import "testing"
+
+func Test_tribonacci(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				n: 37,
+			},
+			want: 2082876103,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tribonacci(tt.args.n); got != tt.want {
+				t.Errorf("tribonacci() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
