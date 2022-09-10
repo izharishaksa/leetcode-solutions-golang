5c266523160b37e6cda3519d56910ef4a0d88889
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-10 09:02:46 +0700

    Create add-two-numbers_test.go

---
+++ add-two-numbers/add-two-numbers_test.go
@@ -0,0 +1,61 @@
+package add_two_numbers
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				l1: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val:  3,
+							Next: nil,
+						},
+					},
+				},
+				l2: &ListNode{
+					Val: 5,
+					Next: &ListNode{
+						Val: 6,
+						Next: &ListNode{
+							Val:  4,
+							Next: nil,
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 7,
+				Next: &ListNode{
+					Val: 0,
+					Next: &ListNode{
+						Val:  8,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
