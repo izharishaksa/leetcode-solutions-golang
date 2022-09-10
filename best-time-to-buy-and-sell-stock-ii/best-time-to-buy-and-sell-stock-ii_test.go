1f75444b9f6aa911a6349692ca2641b57e1a824d
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-10 22:53:27 +0700

    Create best-time-to-buy-and-sell-stock-ii_test.go

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii_test.go
@@ -0,0 +1,29 @@
+package best_time_to_buy_and_sell_stock_ii
+
+import "testing"
+
+func Test_maxProfit(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				prices: []int{1, 8, 3, 10, 5},
+			},
+			want: 14,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfit(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
