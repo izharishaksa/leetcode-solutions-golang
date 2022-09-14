937d10b96ef431c1384f846daa2e971650efaed0
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-14 08:05:51 +0700

    Create powx-n_test.go

---
+++ powx-n_test.go
@@ -0,0 +1,31 @@
+package leetcode_solutions_golang
+
+import "testing"
+
+func Test_myPow(t *testing.T) {
+	type args struct {
+		x float64
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "test case 1",
+			args: args{
+				x: 2.00000,
+				n: 10,
+			},
+			want: 1024.00000,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := myPow(tt.args.x, tt.args.n); got != tt.want {
+				t.Errorf("myPow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
