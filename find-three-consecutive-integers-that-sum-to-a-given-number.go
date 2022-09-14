c5cf823ab77dbc2d7f79a5d799b0c0b00d5f8f64
Izhari Ishak Aksa <izharishaksa@gmail.com>
2022-09-14 08:05:40 +0700

    Update find-three-consecutive-integers-that-sum-to-a-given-number.go

--- find-three-consecutive-integers-that-sum-to-a-given-number.go
+++ find-three-consecutive-integers-that-sum-to-a-given-number.go
@@ -1,6 +1,7 @@
+//https://leetcode.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number/
+
 package leetcode_solutions_golang
 
-//https://leetcode.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number/
 func sumOfThree(num int64) []int64 {
 	mid := num / 3
 	if ((mid - 1) + mid + (mid + 1)) == num {
